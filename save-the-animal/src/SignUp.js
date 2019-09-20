2b84f6e3cb8dbc637f29f94dfd9cc7d3a022d8b6
Greg <remph819@gmail.com>
2019-09-19 17:13:31 -0700

    started signup form

---
+++ save-the-animal/src/SignUp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function SignUp() {
+  return (
+    <div className="Sign-up">
+      <header className="Sign-up-header">
+     <h1>hello</h1>
+      </header>
+    </div>
+  );
+}
+
+export default SignUp;
