e4f2f0429f86c6436e3b1c7e0148326efefafd03
Greg <remph819@gmail.com>
2019-09-19 17:49:43 -0700

    started adding sign-up fields

--- save-the-animal/src/SignUp.js
+++ save-the-animal/src/SignUp.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field} from 'formik';
+import * as yup from 'yup'
+
 import './App.css';
 
 function SignUp() {
+  const [people, setPeople] = useState([])
   return (
-    <div className="Sign-up">
-      <header className="Sign-up-header">
-     <h1>hello</h1>
-      </header>
-    </div>
+   <Form>
+     <Field type="text" name='person' placeholder="*Username" />
+     <Field type="email" name='email' placeholder="*Email" />
+     <Field type="text" name='person' placeholder="*Password" />
+     <Field type="radial" placeholder="Terms of Service" />
+
+
+   </Form>
   );
 }
 
-export default SignUp;
+export default withFormik;
