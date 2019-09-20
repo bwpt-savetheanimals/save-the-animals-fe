e315214203e1852a5d0b8aee651da0b471f78926
Greg <remph819@gmail.com>
2019-09-19 18:55:33 -0700

    created signup form with errors

--- save-the-animal/src/SignUp.js
+++ save-the-animal/src/SignUp.js
@@ -1,21 +1,47 @@
 import React, {useState, useEffect} from 'react';
 import {withFormik, Form, Field} from 'formik';
 import * as yup from 'yup'
-
 import './App.css';
 
-function SignUp() {
+function SignUp({status}) {
+  console.log(status)
   const [people, setPeople] = useState([])
+
+  useEffect(() => {
+    setPeople(...people,status)
+  },[])
   return (
    <Form>
      <Field type="text" name='person' placeholder="*Username" />
+
      <Field type="email" name='email' placeholder="*Email" />
      <Field type="text" name='person' placeholder="*Password" />
-     <Field type="radial" placeholder="Terms of Service" />
+     <label>
+     <Field type="radial" name='Terms of Service' placeholder="Terms of Service" />
+     <span>Term of Service</span>
+     </label>
+     <button>Submit</button>
 
+    
 
    </Form>
   );
 }
 
-export default withFormik;
+export default withFormik({
+  mapPropsToValues: (values) => {
+    return {
+      name: values.name || '',
+      email: values.email || '',
+      password: values.password || '',
+      termsOfService: values.termsOfService || false
+    
+    }
+  },
+  validationSchema: yup.object().shape({
+    name: yup.string().required("Please enter Username"),
+    email:yup.string().required("Please enter valid email"),
+    password:yup.string().required("Please enter password"),
+    termsOfService: yup.bool().required()
+  })
+})(SignUp);
