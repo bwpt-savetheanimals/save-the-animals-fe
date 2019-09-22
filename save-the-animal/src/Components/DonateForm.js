3882f44770d7ad498a927e64d26fd4994df52d9c
Sheila R Moore <sherexmykes@github.com>
2019-09-22 16:27:27 -0400

    added a chat, worked on private route to CampaignDashBoard,add Credit Info to Donate Form

--- save-the-animal/src/Components/DonateForm.js
+++ save-the-animal/src/Components/DonateForm.js
@@ -2,33 +2,39 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-
-
 function DonationForm(errors,touched) {
   return (
     <div><h1>Save The Animals</h1>
-    <Form>
+
+       <Form>
          {touched.username && errors.username && <p>{errors.username}</p>}
       <Field type="text" name="username" placeholder="Username" />
       {touched.donation && errors.donation && <p>{errors.donation}</p>}
       <Field type="number" name="donation" placeholder="donation" />
       {touched.campaign && errors.campaign && <p>{errors.campaign}</p>}
       <Field type="text" name="campaign" placeholder="campaign" />
-      
-      
-      <button>Submit!</button>
+      <Field type="text" name="cardNumber" placeholder="Card Number" />
+      {touched.cardNumber && errors.cardNumber  && <p>{errors.cardNumber}</p>}
+      <Field type="date" name="expiryDate" placeholder="Expiration Date" />
+      {touched.expiryDate && errors.expiryDate  && <p>{errors.expiryDate}</p>}
+      <Field type="text" name="CVC/CVV" placeholder="CVC/CVV Number" />
+      {touched.cvc && errors.cvc  && <p>{errors.cvc}</p>}
+
+      <button class="button-credit-card" onClick="submitCreditCardClick(event)">Donate!</button>
 
     </Form></div>
   );
 }
 const FormikDonationForm = withFormik({
 
-    mapPropsToValues({ username, donation, campaign }) {
+    mapPropsToValues({ username, donation, campaign, cardNumber,expiryDate,cvc }) {
       return {
         username: username || "",
         donation: donation || "",
         campaign: campaign || "",
-       
+        cardNumber: cardNumber || "",
+        expiryDate: expiryDate || "",
+        cvc: cvc|| "",
     };
 },
 validationSchema: Yup.object().shape({
@@ -40,7 +46,21 @@ validationSchema: Yup.object().shape({
     .required("Please Submit A Dollar Amount"),
       campaign: Yup.string()
     
-      .required("Please pick a Campaign")
+      .required("Please pick a Campaign"),
+
+      cardNumber: Yup.string()
+      .min (16)
+      .required("Please Enter Valid Card Number"),
+
+      expiryDate: Yup.date()
+      
+      .required("Please Enter Valid Date"),
+
+      cvc : Yup.string()
+      .min (3)
+      .required("Please Enter Valid CVC/CVV Number"),
+
+
   }),
 
 
@@ -50,6 +70,5 @@ handleSubmit(values) {
   }
   
     })(DonationForm);
-
 
 export default FormikDonationForm;
