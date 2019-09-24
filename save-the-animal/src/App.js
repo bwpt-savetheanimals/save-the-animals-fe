1ffc18c26a6d0f210e0aaa74251299e514d50844
Greg <remph819@gmail.com>
2019-09-23 19:59:38 -0700

    worked on signup page

--- save-the-animal/src/App.js
+++ save-the-animal/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import DonateForm from './Components/DonateForm'
 import Chat from './Components/Chat'
-import SignUp from './SignUp'
+import SignUpForm from './SignUp'
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
@@ -14,21 +14,22 @@ import Data from './Components/Campaign/CampaignData';
 import PrivateRouteCD from './Components/PrivateRouteCD';
 import CampaignLogin from './Components/CampaignLogin';
 
+
 function App() {
   return (
 
     <div className="App">
 
 
-      <Navigation />
+      
      
       <Route exact path="/" component={WelcomePage} />
-      <Route path="/signup" component={SignUp} />
+      <Route path="/signup" component={SignUpForm} />
       <Route path="/login" component={FormikUserForm} />
       <Route path="/dashboard" component={Dashboard}></Route> 
       <Route path="/Cdashboard" component={CampaignDashboard}> <Data/> </Route>
       <Route path="/CampaignLogin" component={CampaignLogin}/>
-      <PrivateRouteCD path="/Cdashboard" component={CampaignDashboard}><Data/> </Route> 
+      <PrivateRouteCD path="/Cdashboard" component={CampaignDashboard}><Data/> </PrivateRouteCD> 
       <Route path="/donate" component={DonateForm}/>
      <Route path="/chat"component={Chat}/>
      <Route path="/campaign" component={CampaignForm}/>
