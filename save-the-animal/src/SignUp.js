be97eedd2c0603f9349670e3cb829052254d6eb4
Greg <remph819@gmail.com>
2019-09-23 20:00:16 -0700

    saved signup

--- save-the-animal/src/SignUp.js
+++ save-the-animal/src/SignUp.js
@@ -20,50 +20,49 @@ const StyledButton = styled.button`
 	margin: 5px;
 `;
 
-
-
-
-
-
-
-
 function SignUp({ status, touched, errors }) {
-		console.log(status);
-		const [ people, setPeople ] = useState([]);
+	console.log(status);
+	const [ people, setPeople ] = useState([]);
 
-		useEffect(
-			() => {
-				if (status) setPeople(...people, status);
-			},
-			[ people ]
-		);
-		return (
-			<div className="signupForm">
-				<Form>
-					<h1>Sign Up</h1>
-					<div>
-						{touched.username && errors.username && <p className="error"> {errors.username}</p>}
-						<StyledField type="text" name="username" placeholder="*Username" />
-					</div>
-					<div>
-						{touched.email && errors.email && <p className="error"> {errors.email}</p>}
-						<StyledField type="email" name="email" placeholder="*Email" />
-					</div>
-					<div>
-						{touched.password && errors.password && <p className="error"> {errors.password}</p>}
-						<StyledField type="password" name="person" placeholder="*Password" />
-					</div>
-          <div class="form-group form-check">
-					<label class="form-check-label" for="exampleCheck1" >
-						<StyledField class="form-check-input" id="exampleCheck1" type="checkbox" name="Terms of Service" placeholder="Terms of Service" />
+	useEffect(
+		() => {
+			if (status) setPeople(...people, status);
+		},
+		[ people ]
+	);
+	return (
+		<div className="signupForm">
+			<Form>
+				<h1>Sign Up</h1>
+				<div>
+					{touched.username && errors.username && <p className="error"> {errors.username}</p>}
+					<StyledField type="text" name="username" placeholder="*Username" />
+				</div>
+				<div>
+					{touched.email && errors.email && <p className="error"> {errors.email}</p>}
+					<StyledField type="email" name="email" placeholder="*Email" />
+				</div>
+				<div>
+					{touched.password && errors.password && <p className="error"> {errors.password}</p>}
+					<StyledField type="password" name="person" placeholder="*Password" />
+				</div>
+				<div class="form-group form-check">
+					<label class="form-check-label" for="exampleCheck1">
+						<StyledField
+							class="form-check-input"
+							id="exampleCheck1"
+							type="checkbox"
+							name="Terms of Service"
+							placeholder="Terms of Service"
+						/>
 						<span>Term of Service</span>
 					</label>
-          </div>
-					<StyledButton type="submit">Submit</StyledButton>
-				</Form>
-			</div>
-		);
-	}
+				</div>
+				<StyledButton type="submit">Submit</StyledButton>
+			</Form>
+		</div>
+	);
+}
 
 const SignUpForm = withFormik({
 	mapPropsToValues: ({ username, email, password, termsOfService }) => {
@@ -73,17 +72,16 @@ const SignUpForm = withFormik({
 			password: password || '',
 			termsOfService: termsOfService || false
 		};
-  },
-  validationSchema: yup.object().shape({
-    name: yup.string().required('Please enter Username'),
-    email: yup.string().email('Please enter valid email'),
-    password: yup.string().min('8 characters minimum').matches(/(?=.*[0-9])/),
-    termsOfService: yup.bool().required()
-  }),
-  
+	},
+	validationSchema: yup.object().shape({
+		name: yup.string().required('Please enter Username'),
+		email: yup.string().email('Please enter valid email'),
+		password: yup.string().min('8 characters minimum').matches(/(?=.*[0-9])/),
+		termsOfService: yup.bool().required()
+	}),
 
 	handleSubmit: (values, { props, setStatus }) => {
-    props.history.push('/dashboard');
+		props.history.push('/dashboard');
 		axios
 			.post('https://reqres.in/api/users', values)
 			.then((res) => {
